a76daf2debe210d525d6af2ffa6d2875e96c526b
sanket9162 <sanketgondhali55@gmail.com>
2025-01-29 22:08:16 +0530

    install chi

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sanket9162/lenslocked
 
 go 1.23.4
+
+require github.com/go-chi/chi/v5 v5.2.0 // indirect
--- main.go
+++ main.go
@@ -6,12 +6,29 @@ import (
 )
 
 
-func handlerFunc(w http.ResponseWriter, r *http.Request){
+func homeHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1> hello from go</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w,"<h1> hello from Contact page</h1>" )
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w , `<h1>FAQ Page</h1>
+	
+`)
+}
+
+
 func main(){
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/faq", faqHandler)
+
 	fmt.Println("staring the server on :3000")
 	http.ListenAndServe(":3000",nil)
 
