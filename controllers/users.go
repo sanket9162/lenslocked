9a66f43e438b29ad41d9d1f2e5f5684780abfc66
sanket9162 <sanketgondhali55@gmail.com>
2025-02-27 21:21:43 +0530

    create cookie

--- controllers/users.go
+++ controllers/users.go
@@ -59,6 +59,22 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request){
 		return
 
 	}
+	cookie := http.Cookie{
+		Name: "email",
+		Value: user.Email,
+		Path: "/",
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &cookie)
 	fmt.Fprintf(w, "user authenticated: %+v", user)
 
+}
+
+func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request){
+	email, err:= r.Cookie("email")
+	if err != nil {
+		fmt.Fprint(w, "The email cookie could not be read.")
+		return 
+	}
+	fmt.Fprintf(w, "Email cookie : %s\n", email.Value)
 }
