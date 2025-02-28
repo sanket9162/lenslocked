55aab8d6d8cbb695233bd9e40d61627c3590051f
sanket9162 <sanketgondhali55@gmail.com>
2025-02-28 11:13:11 +0530

    Request specific CSRF template func

--- views/template.go
+++ views/template.go
@@ -6,6 +6,8 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/csrf"
 )
 type Template struct {
 	htmlTpl *template.Template
@@ -24,7 +26,7 @@ func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
 	tpl = tpl.Funcs(
 		template.FuncMap{
 			"csrfFeild": func() template.HTML {
-				return `<input type="hidden"/>`
+				return ""
 			},
 		},
 	)
@@ -38,10 +40,22 @@ func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
 	}, nil
 }
 
-func (t Template) Execute (w http.ResponseWriter, data interface{}){
+func (t Template) Execute (w http.ResponseWriter, r *http.Request, data interface{}){
+	tpl, err := t.htmlTpl.Clone()
+	if err != nil {
+		log.Printf("Cloning template: %v", err)
+		http.Error(w, "There was an error rendering the page.", http.StatusInternalServerError)
+		return
+	}
+	tpl = tpl.Funcs(
+		template.FuncMap{
+			"csrfFeild": func() template.HTML {
+				return csrf.TemplateField(r)
+			},
+		},
+	)
 	w.Header().Set("content-Type", "text/html; charset=utf-8")
-
-	err := t.htmlTpl.Execute(w, data)
+	err = tpl.Execute(w, data)
 	if err != nil{
 		log.Printf("executing  template : %v", err)
 		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
