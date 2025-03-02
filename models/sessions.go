4ae997f08fc357ae1acf09db8d05afec597a8e9d
sanket9162 <sanketgondhali55@gmail.com>
2025-03-02 10:59:41 +0530

    refactor rand package

--- models/sessions.go
+++ models/sessions.go
@@ -6,6 +6,11 @@ import (
 
 	"github.com/sanket9162/lenslocked/rand"
 )
+
+const (
+	MinBytesPerToken = 32
+)
+
  type Session struct{
 	ID int 
 	UserID int 
@@ -15,10 +20,15 @@ import (
 
  type SessionService struct{
 	DB *sql.DB
+	BytesPerToken int
  }
 
  func (ss *SessionService) Create(userID int) (*Session, error) {
-	token, err := rand.SessionToken()
+	bytesPerToken := ss.BytesPerToken
+	if bytesPerToken < MinBytesPerToken{
+		bytesPerToken = MinBytesPerToken
+	}
+	token, err := rand.String(bytesPerToken)
 	if err != nil {
 		return nil, fmt.Errorf("create: %w", err)
 	}
