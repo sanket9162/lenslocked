81028f63e30421ef114b76a57f4f8d91ba17579e
sanket9162 <sanketgondhali55@gmail.com>
2025-03-02 11:13:00 +0530

    hash session tokens

--- models/sessions.go
+++ models/sessions.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
 
 	"github.com/sanket9162/lenslocked/rand"
@@ -35,10 +37,16 @@ const (
 	session := Session{
 		UserID: userID,
 		Token: token,
+		TokenHash: ss.hash(token),
 	}
 	return &session,nil
  } 
 
  func (ss *SessionService) User(token string) (*User, error){
 	return nil, nil
+ }
+
+ func (ss *SessionService) hash(token string) string{
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
  }
