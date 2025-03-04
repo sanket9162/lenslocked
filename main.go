0a16e51c71c416e5c73b7adc35cbe56766591652
sanket9162 <sanketgondhali55@gmail.com>
2025-03-04 13:46:56 +0530

    using goose

--- main.go
+++ main.go
@@ -30,6 +30,12 @@ func main(){
 		panic(err)
 	} 
 	defer db.Close()
+
+	err = models.Migrate(db, "migrations")
+	if err != nil{
+		panic(err)
+	}
+
 	userService := models.UserService{
 		DB: db,
 	}
