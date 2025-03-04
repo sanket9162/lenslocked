0f8c3014a2976a321736e1619ae1881b674147d4
sanket9162 <sanketgondhali55@gmail.com>
2025-03-04 09:37:33 +0530

    using join in sessionService

--- models/sessions.go
+++ models/sessions.go
@@ -59,25 +59,20 @@ const (
 	return &session,nil
  } 
 
- func (ss *SessionService) User(token string) (*User, error){
+  func (ss *SessionService) User(token string) (*User, error){
 
 	tokenHash := ss.hash(token)
 	var user User
 	row := ss.DB.QueryRow(`
-	SELECT user_id
+	SELECT user_id,
+	users.email,
+	users.password_hash
 	FROM sessions
-	WHERE token_hash = $1;`, tokenHash)
+	JOIN users ON users.id = sessions.user_id
+	WHERE sessions.token_hash = $1;`, tokenHash)
 
-	err := row.Scan(&user.ID)
+	err := row.Scan(&user.ID, &user.Email, &user.PasswordHash)
 	if err != nil{
-		return nil, fmt.Errorf("user: %w", err)
-	}
-
-	row  = ss.DB.QueryRow(`
-	SELECT email, password_hash
-	FROM users WHERE id = $1;`, user.ID)
-	err = row.Scan(&user.Email, &user.PasswordHash)
-	if err != nil {
 		return nil, fmt.Errorf("user: %w", err)
 	}
 	return &user,nil
