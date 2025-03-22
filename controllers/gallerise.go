2da595b347493cbca36cbf041fc592676ab1661e
sanket9162 <sanketgondhali55@gmail.com>
2025-03-22 20:35:42 +0530

    gallery check with func opts

--- controllers/gallerise.go
+++ controllers/gallerise.go
@@ -71,45 +71,36 @@ func (g Galleries) Show(w http.ResponseWriter, r *http.Request){
 }
 
 
-func (g Galleries) Edit (w http.ResponseWriter, r *http.Request){
-	gallery, err := g.galleryById(w, r)
+func (g Galleries) Edit(w http.ResponseWriter, r *http.Request){
+	gallery, err := g.galleryById(w, r, userMustOwnGallery)
 	if err != nil {
 		return
 	}
-user := context.User(r.Context())
-if gallery.UserID != user.ID {
-	http.Error(w, "You are not authorized to edit this gallery", http.StatusForbidden)
-	return
-}
-var data struct {
-	ID int 
-	Title string
-}
+	var data struct {
+		ID int 
+		Title string
+	}
 
-data.ID = gallery.ID
-data.Title = gallery.Title
-g.Templates.Edit.Execute(w, r, data)
+	data.ID = gallery.ID
+	data.Title = gallery.Title
+	g.Templates.Edit.Execute(w, r, data)
 
 }
 
 func (g Galleries) Update(w http.ResponseWriter, r *http.Request){
-	gallery, err := g.galleryById(w, r)
+	gallery, err := g.galleryById(w, r, userMustOwnGallery)
 	if err != nil {
 		return
 	}
-user := context.User(r.Context())
-if gallery.UserID != user.ID {
-	http.Error(w, "You are not authorized to edit this gallery", http.StatusForbidden)
-	return
-}
-gallery.Title = r.FormValue("title")
-err = g.GalleryService.Update(gallery)
-if err != nil {
-	http.Error(w, "Something went wrong", http.StatusInternalServerError)
-	return
-}
-editPath := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
-http.Redirect(w, r, editPath, http.StatusFound)
+
+	gallery.Title = r.FormValue("title")
+	err = g.GalleryService.Update(gallery)
+	if err != nil {
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+	editPath := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
+	http.Redirect(w, r, editPath, http.StatusFound)
 
 }
 
@@ -137,7 +128,10 @@ func (g Galleries) Index(w http.ResponseWriter, r *http.Request){
 	g.Templates.Index.Execute(w, r,data)
 }
 
-func (g Galleries) galleryById(w http.ResponseWriter, r *http.Request) (*models.Gallery, error){
+type galleryOpt func(http.ResponseWriter, *http.Request, *models.Gallery) error  
+
+
+func (g Galleries) galleryById(w http.ResponseWriter, r *http.Request, opts ...galleryOpt) (*models.Gallery, error){
 	id, err := strconv.Atoi(chi.URLParam(r, "id"))
 	if err != nil {
 		http.Error(w, "Invalid ID", http.StatusNotFound)
@@ -153,5 +147,21 @@ func (g Galleries) galleryById(w http.ResponseWriter, r *http.Request) (*models.
 	http.Error(w, "something went wrong", http.StatusInternalServerError)
 	return nil, err
 	}
+	for _, opt := range opts {
+		err = opt(w, r, gallery)
+		if err != nil {
+			return nil, err
+		}
+	}
 	return gallery, nil
+}
+
+
+func userMustOwnGallery(w http.ResponseWriter, r *http.Request, gallery *models.Gallery) error {
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "You are not authorized to edit this gallery", http.StatusForbidden)
+		return fmt.Errorf("user does not have access to this gallery")
+	}
+	return nil
 }
